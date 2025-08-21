96463e38ebc555af4220f4873c8b28b1e8036f3b
fykabutch <fuckabuka@mail.ru>
2025-08-21 16:45:47 +0500

    Тест: устранение белого экрана pages. Версия 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/test-project/",
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   }
 })
