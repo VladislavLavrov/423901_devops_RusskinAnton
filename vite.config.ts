02fd888d94f0b52612902d52a8a0e81542e1c6ba
fykabutch <fuckabuka@mail.ru>
2025-08-30 10:57:03 +0500

    Коммит 7: Добавление build в vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bars-web-application",
+  base: "./",
+    build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 })
