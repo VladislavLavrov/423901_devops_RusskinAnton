41131d1aaac2819b136f8bb8a9805d3f47e872d6
fykabutch <fuckabuka@mail.ru>
2025-08-30 14:51:02 +0500

    Удаление файлов пакетов и изменения в .gitignore и vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./",
-    build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
-  }
 })
